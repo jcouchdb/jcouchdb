17ae705e1c55bb35356e7befee675cebc683845b
Steve Moyer <smoyer1@selesy.com>
2017-04-29 16:30:08 -0400

    Fixes misuse of Hamcrest anyOf matcher.

--- src/test/java/org/jcouchdb/util/CouchDBUpdaterTestCase.java
+++ src/test/java/org/jcouchdb/util/CouchDBUpdaterTestCase.java
@@ -1,8 +1,8 @@
 package org.jcouchdb.util;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isOneOf;
 import static org.hamcrest.Matchers.notNullValue;
 
 import java.io.File;
@@ -17,65 +17,44 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+public class CouchDBUpdaterTestCase {
+	private final static Logger log = LoggerFactory.getLogger(CouchDBUpdaterTestCase.class);
 
-public class CouchDBUpdaterTestCase
-{
-    private final static Logger log = LoggerFactory.getLogger(CouchDBUpdaterTestCase.class);
+	@Test
+	public void thatDesignDocumentReadingWorks() throws IOException {
 
-    @Test
-    public void thatDesignDocumentReadingWorks() throws IOException
-    {
+		CouchDBUpdater couchDBUpdater = new CouchDBUpdater();
+		couchDBUpdater.setDesignDocumentDir(new File("src/test/resources/org/jcouchdb/util/test-views/"));
+		couchDBUpdater.setDatabase(LocalDatabaseTestCase.createDatabaseForTest());
 
-        
-        CouchDBUpdater couchDBUpdater = new CouchDBUpdater();
-        couchDBUpdater.setDesignDocumentDir(new File("src/test/resources/org/jcouchdb/util/test-views/"));
-        couchDBUpdater.setDatabase(LocalDatabaseTestCase.createDatabaseForTest());
+		List<DesignDocument> docs = couchDBUpdater.updateDesignDocuments();
+		testDocsIntegrity(docs);
+	}
 
+	void testDocsIntegrity(List<DesignDocument> docs) {
+		final String mapFn = "function()\n" + "{\n" + "\tif (doc.type == \"foo\")\n" + "\t{\n"
+				+ "\t\temit(doc.id,null);\n" + "\t}\n" + "}\n";
 
-        List<DesignDocument> docs = couchDBUpdater.updateDesignDocuments();
-        testDocsIntegrity( docs);
-    }
+		assertThat(docs, is(notNullValue()));
+		assertThat(docs.size(), is(2));
 
-    void testDocsIntegrity( List<DesignDocument> docs)
-    {
-        final String mapFn = 
-            "function()\n" + 
-            "{\n" + 
-            "\tif (doc.type == \"foo\")\n" + 
-            "\t{\n" + 
-            "\t\temit(doc.id,null);\n" + 
-            "\t}\n" + 
-            "}\n"; 
+		for (DesignDocument doc : docs) {
+			Map<String, View> views = doc.getViews();
+			assertThat(views.size(), is(1));
 
-        assertThat(docs,is(notNullValue()));
-        assertThat(docs.size(), is(2));
-        
-        for (DesignDocument doc : docs)
-        {
-            Map<String, View> views = doc.getViews();
-            assertThat(views.size(), is(1));
-            
-            View view = views.get("foo");
-            
-            if (doc.getId().equals("_design/view1"))
-            {
-                assertThat(view.getMap(),is(mapFn));
-            }
-            else if (doc.getId().equals("_design/view2/sub"))
-            {
-                assertThat(view.getMap(),is(mapFn));
-                assertThat(view.getReduce(),is("function()\n" + 
-                    "{\n" + 
-                    "\treturn 0;\n" + 
-                "}\n"));
-            }
-            else
-            {
-                assertThat("invalid id " + doc.getId(), false, is(true));
-            }
-        }
-        
-        assertThat(docs.get(0).getId(),anyOf( is("_design/view1"), is("_design/view2/sub")));
-        assertThat(docs.get(1).getId(),anyOf( is("_design/view1"), is("_design/view2/sub")));
-    }
+			View view = views.get("foo");
+
+			if (doc.getId().equals("_design/view1")) {
+				assertThat(view.getMap(), is(mapFn));
+			} else if (doc.getId().equals("_design/view2/sub")) {
+				assertThat(view.getMap(), is(mapFn));
+				assertThat(view.getReduce(), is("function()\n" + "{\n" + "\treturn 0;\n" + "}\n"));
+			} else {
+				assertThat("invalid id " + doc.getId(), false, is(true));
+			}
+		}
+
+		assertThat(docs.get(0).getId(), isOneOf("_design/view1", "_design/view2/sub"));
+		assertThat(docs.get(1).getId(), isOneOf("_design/view1", "_design/view2/sub"));
+	}
 }
